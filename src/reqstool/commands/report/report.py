339b9e9f056c8869d6eb1e03915b9a58b24e8a29
David Mathias Mortensen <david.mortensen@lfv.se>
2025-04-01 14:41:30 +0200

    fix: remove whitespace to make flake8 happy

--- src/reqstool/commands/report/report.py
+++ src/reqstool/commands/report/report.py
@@ -108,7 +108,7 @@ class ReportCommand:
         asciidoc: str = "= REQUIREMENTS DOCUMENTATION\n" + statistics_table
 
         for group_by in template_data.keys():
-            asciidoc += f"== {group_by[0].upper() + group_by[1:] }\n"
+            asciidoc += f"== {group_by[0].upper() + group_by[1:]}\n"
 
             for template in template_data[group_by]:
 
